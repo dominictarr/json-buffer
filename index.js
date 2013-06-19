2b7168b10f03cee9c3b9b339c3729b1b40c84dbb
Dominic Tarr <dominic.tarr@gmail.com>
2013-06-19 11:02:42 +0100

    use bops so works in the browser!

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-var Buffer = require('buffer').Buffer
+var bops = require('bops')
 
 //TODO: handle reviver/dehydrate function like normal
 //and handle indentation, like normal.
 //if anyone needs this... please send pull request.
 
 exports.stringify = function stringify (o) {
-  if(o && Buffer.isBuffer(o))
+  if(o && bops.is(o))
     return JSON.stringify(':base64:' + o.toString('base64'))
 
   if(o && o.toJSON)
@@ -39,7 +39,7 @@ exports.parse = function (s) {
   return JSON.parse(s, function (key, value) {
     if('string' === typeof value) {
       if(/^:base64:/.test(value))
-        return new Buffer(value.substring(8), 'base64')
+        return bops.from(value.substring(8), 'base64')
       else
         return /^:/.test(value) ? value.substring(1) : value 
     }
